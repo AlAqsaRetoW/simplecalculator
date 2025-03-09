c79fcc5ba757f5bf6b2f0b94ca8ebf3a001c59b9
Aqsa <al.aqsarw@gmail.com>
2025-03-09 23:49:49 +0700

    add comma function

--- app.js
+++ app.js
@@ -11,16 +11,52 @@ const btnSeven = document.querySelector("#btnSeven");
 const btnEight = document.querySelector("#btnEight");
 const btnNine = document.querySelector("#btnNine");
 
+const isOperator = (char) => {
+    return ['+', '-', '*', '/', '%'].includes(char);
+}
+
+const operator = (operator) => {
+    const lastChar = display.innerText.slice(-1);
+
+    if (!isOperator(lastChar)) {
+        display.innerText += operator;
+    }
+}
+
 const calc = (num) => {
     if (display.innerText == "0") {
-      display.innerText = num;
+        display.innerText = num;
     } else {
-      display.innerText += num;
+        display.innerText += num;
     }
-  }
+}
 
 const add = () => {
-    display.innerText += "+"
+    operator("+");
+}
+
+const subtract = () => {
+    operator("-");
+}
+
+const multiply = () => {
+    operator("*");
+}
+
+const divide = () => {
+    operator("/");
+}
+
+const percent = () => {
+    operator("%");
+}
+
+const reset = () => {
+    display.innerText = "0";
+}
+
+const point = () => {
+    display.innerText += "."
 }
 
 const res = () => {
